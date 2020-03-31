b2a453fbecbe1aad6f9353921b8451c172abdba9
Joubert Guimarães de Assis <me+github@redrat.com.br>
2020-03-31 14:40:20 -0300

    fix indentation

--- src/components/button/ButtonBase.jsx
+++ src/components/button/ButtonBase.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import './ButtonBase.css'
 
 export default props =>
-    <button
-        className={`
-            button
-            ${props.operation ? 'operation' : ''}
-            ${props.double ? 'double' : ''}
-            ${props.triple ? 'triple' : ''}
-        `}
-        onClick={e => props.click && props.click(props.label)}
-    >
-        {props.label}
-    </button>
+  <button
+    className={`
+      button
+      ${props.operation ? 'operation' : ''}
+      ${props.double ? 'double' : ''}
+      ${props.triple ? 'triple' : ''}
+    `}
+    onClick={e => props.click && props.click(props.label)}
+  >
+    {props.label}
+  </button>
